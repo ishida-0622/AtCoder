183dcee2f710ec5bda4972cfb3cbfae6dae9587b
kentaro ishida <ken.babuo@gmail.com>
2022-06-22 14:06:36 +0900

    add:205_A~C

---
+++ ABC/201-210/205/C.py
@@ -0,0 +1,35 @@
+import sys
+inputSys = sys.stdin.readline
+sys.setrecursionlimit(10**7)
+from copy import deepcopy
+from collections import deque
+import itertools  # combinations() <- nCr, permutations() <- nPr
+import bisect
+import math
+from functools import lru_cache
+import string
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+# lst.sort(key=lambda x: x[N]) <- N番目の要素でソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[0]) <- 辞書のkeyでソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[1]) <- 辞書のvalueでソート
+
+
+
+a, b, c = map(int, inputSys().split())
+if a == b or abs(a) == abs(b) and c % 2 == 0:
+    print("=")
+    exit()
+if c % 2:
+    if a > b:
+        print(">")
+    else:
+        print("<")
+else:
+    if abs(a) > abs(b):
+        print(">")
+    else:
+        print("<")
