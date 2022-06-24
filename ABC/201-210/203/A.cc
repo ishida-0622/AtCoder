36fdc04756e6e9eb76e3df3bd06318466f63bcd4
kentaro ishida <ken.babuo@gmail.com>
2022-06-24 16:52:39 +0900

    add:203_A_お嬢様

---
+++ ABC/201-210/203/A.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define test auto
+#define は以下のことをなさいます {
+#define 以上ですわ }
+#define お返しするのは return
+#define ですわ ;
+#define もし if(
+#define でしたら ){
+#define そうでなく }else
+#define いずれでもなければ }else{
+#define そうでなければ }else{
+#define こちらの auto
+#define 様は =
+#define 繰り返しを for(int i = 0; i <
+#define 回行いますわ ; i++){
+#define 標準出力に cout <<
+#define と表示いたしますわ << endl;
+#define を増やしますわ ++;
+#define かつ &&
+#define もしくは ||
+#define 整数の int
+#define 標準入力から cin >>
+#define と >>
+#define を受け取りますわ ;
+#define ぶーるのべくたー(x) vector<bool>(x);
+#define えー a
+#define びー b
+#define しー c
+#define ぜろ 0
+#define そして ,
+
+using namespace std;
+
+整数の main() は以下のことをなさいます
+    整数の えー そして びー そして しー ですわ
+    標準入力から えー と びー と しー を受け取りますわ
+    もし えー != びー かつ えー != しー かつ びー != しー でしたら
+        標準出力に ぜろ と表示いたしますわ
+    そうでなく もし えー == びー でしたら
+        標準出力に しー と表示いたしますわ
+    そうでなく もし えー == しー でしたら
+        標準出力に びー と表示いたしますわ
+    そうでなく もし びー == しー でしたら
+        標準出力に えー と表示いたしますわ
+    以上ですわ
+    お返しするのは ぜろ ですわ
+以上ですわ
