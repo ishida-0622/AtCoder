bfa2d01591e25870b6575cd5f74f486cc2d97624
kentaro ishida <ken.babuo@gmail.com>
2022-06-24 16:54:26 +0900

    add:203_B

---
+++ ABC/201-210/203/B.py
@@ -0,0 +1,27 @@
+import sys
+inputSys = sys.stdin.readline
+sys.setrecursionlimit(10**7)
+from copy import deepcopy
+from collections import deque
+import itertools  # combinations() <- nCr, permutations() <- nPr
+import bisect
+import math
+from functools import lru_cache
+import string
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+# lst.sort(key=lambda x: x[N]) <- N番目の要素でソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[0]) <- 辞書のkeyでソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[1]) <- 辞書のvalueでソート
+
+
+
+n, k = map(int, inputSys().split())
+ans = 0
+for i in range(1,n+1):
+    for j in range(1, k+1):
+        ans += int(str(i) + "0" + str(j))
+print(ans)
