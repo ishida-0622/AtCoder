1a01e0aefe4a5bd94fbff73a8b90e0c103172164
kentaro ishida <ken.babuo@gmail.com>
2022-09-12 11:33:29 +0900

    add:148_B

---
+++ ARC/148/B.py
@@ -0,0 +1,34 @@
+import sys
+inputSys = sys.stdin.readline
+sys.setrecursionlimit(10**7)
+from copy import deepcopy
+from collections import deque
+import itertools  # combinations() <- nCr, permutations() <- nPr
+import bisect
+import math
+from functools import lru_cache
+import string
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+# lst.sort(key=lambda x: x[N]) <- N番目の要素でソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[0]) <- 辞書のkeyでソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[1]) <- 辞書のvalueでソート
+
+
+
+n = int(inputSys())
+s = inputSys().rstrip()
+start = 0
+
+while start < n and s[start] == "d":
+    start += 1
+
+ans = deepcopy(s)
+
+for i in range(start+1, n+1):
+    ans = min(ans, s[:start] + "".join(reversed(["d" if a == "p" else "p" for a in s[start:i]])) + s[i:])
+
+print(ans)
