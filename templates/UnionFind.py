3496ee34ce8c92520cf5b4c5d9f8333f49d96345
kentaro ishida <ken.babuo@gmail.com>
2022-10-19 21:45:13 +0900

    add: Union-Find

---
+++ templates/UnionFind.py
@@ -0,0 +1,30 @@
+class UnionFind():
+    def __init__(self, n: int):
+        self.parents = [-1] * n
+        self.rank = [0] * n
+        self.siz = [1] * n
+
+    def find(self, x: int):
+        if self.parents[x] == -1:
+            return x
+        self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+
+    def union(self, x: int, y: int):
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:
+            return False
+        if self.rank[x] < self.rank[y]:
+            x, y = y, x
+        self.parents[y] = x
+        self.siz[x] += self.siz[y]
+        if self.rank[x] == self.rank[y]:
+            self.rank[x] += 1
+        return True
+
+    def same(self, x: int, y: int):
+        return self.find(x) == self.find(y)
+
+    def size(self, x: int):
+        return self.siz[self.find(x)]
