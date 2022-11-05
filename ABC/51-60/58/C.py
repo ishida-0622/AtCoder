a872948f850ec581128a1ef4a7d0e6c58fb99b45
kentaro ishida <ken.babuo@gmail.com>
2022-11-05 17:45:08 +0900

    add:58_A~C

---
+++ ABC/51-60/58/C.py
@@ -0,0 +1,47 @@
+import sys
+inputSys = sys.stdin.readline
+sys.setrecursionlimit(10**7)
+import bisect
+import string
+import math
+from math import gcd
+from copy import deepcopy
+from collections import deque
+from collections import defaultdict
+from itertools import combinations
+from itertools import permutations
+from itertools import accumulate
+from functools import lru_cache
+from decimal import Decimal, ROUND_HALF_UP
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+max___ = max
+___min = min
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def lcm(a,b): return a*b // gcd(a,b)
+# lst.sort(key=lambda x: x[N]) <- N番目の要素でソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[0]) <- 辞書のkeyでソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[1]) <- 辞書のvalueでソート
+# Decimal(str(n)).quantize(Decimal("0.1"), ROUND_HALF_UP) <- 四捨五入
+
+
+
+n = int(inputSys())
+lst = [input() for _ in range(n)]
+
+dic = {v: inf for v in string.ascii_lowercase}
+
+for val in lst:
+    d = {v: 0 for v in string.ascii_lowercase}
+    for v in val:
+        d[v] += 1
+    for k, v in d.items():
+        dic[k] = min(dic[k], v)
+
+ans = ""
+for key, val in sorted(dic.items(), key=lambda x: x[0]):
+    ans += str(key) * val
+
+print(ans)
