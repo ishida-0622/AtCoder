61668b61eeb16558c3731e6ed2afffb8b7c7293a
kentaro ishida <ken.babuo@gmail.com>
2022-11-22 10:47:49 +0900

    add:152_A

---
+++ ARC/152/A.py
@@ -0,0 +1,44 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10**7)
+import bisect
+import string
+import math
+from math import gcd
+from copy import deepcopy
+from collections import deque, defaultdict
+from itertools import combinations, permutations, accumulate
+from functools import lru_cache
+from decimal import Decimal, ROUND_HALF_UP
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+max___ = max
+___min = min
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def lcm(a,b): return a*b // gcd(a,b)
+# lst.sort(key=lambda x: x[N]) <- N番目の要素でソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[0]) <- 辞書のkeyでソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[1]) <- 辞書のvalueでソート
+# Decimal(str(n)).quantize(Decimal("0.1"), ROUND_HALF_UP) <- 四捨五入
+
+
+
+N, L = map(int, input().split())
+lst = list(map(int, input().split()))
+ans = True
+if L <= 2:
+    print("Yes")
+    exit()
+idx = 1
+for v in lst:
+    if v == 1:
+        idx = min(idx+2, L)
+    else:
+        if idx+1 <= L:
+            idx = min(idx+3, L)
+        else:
+            ans = False
+
+YesNo(ans)
