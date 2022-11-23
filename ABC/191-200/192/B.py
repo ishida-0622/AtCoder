ef3fad36084f2d50711c97a8e687c8df5a772766
kentaro ishida <ken.babuo@gmail.com>
2022-11-23 15:51:41 +0900

    add:192_A~C

---
+++ ABC/191-200/192/B.py
@@ -0,0 +1,34 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10**7)
+import bisect
+import string
+import math
+from math import gcd
+from copy import deepcopy
+from collections import deque, defaultdict
+from itertools import combinations, permutations, accumulate
+from functools import lru_cache
+from decimal import Decimal, ROUND_HALF_UP
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+max___ = max
+___min = min
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def lcm(a,b): return a*b // gcd(a,b)
+# lst.sort(key=lambda x: x[N]) <- N番目の要素でソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[0]) <- 辞書のkeyでソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[1]) <- 辞書のvalueでソート
+# Decimal(str(n)).quantize(Decimal("0.1"), ROUND_HALF_UP) <- 四捨五入
+
+
+
+s = input()
+a_z = string.ascii_lowercase
+A_Z = string.ascii_uppercase
+if len(s) == 1:
+    YesNo(s in a_z)
+else:
+    YesNo(min([1 if v in a_z else 0 for v in s[::2]]) and min([1 if v in A_Z else 0 for v in s[1::2]]))
