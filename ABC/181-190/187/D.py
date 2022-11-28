0c638fd24905b7e4b711edf559c25f5cef3d81d8
kentaro ishida <ken.babuo@gmail.com>
2022-11-28 21:23:10 +0900

    add:187_D

---
+++ ABC/181-190/187/D.py
@@ -0,0 +1,47 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10**7)
+import bisect
+import string
+import math
+from math import gcd
+from copy import deepcopy
+from collections import deque, defaultdict
+from itertools import combinations, permutations, accumulate
+from functools import lru_cache
+from decimal import Decimal, ROUND_HALF_UP
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+max___ = max
+___min = min
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def lcm(a,b): return a*b // gcd(a,b)
+# lst.sort(key=lambda x: x[N]) <- N番目の要素でソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[0]) <- 辞書のkeyでソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[1]) <- 辞書のvalueでソート
+# Decimal(str(n)).quantize(Decimal("0.1"), ROUND_HALF_UP) <- 四捨五入
+
+
+
+n = int(input())
+lst = []
+aoki = 0
+takahashi = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    aoki += a
+    lst.append([a,b])
+
+lst.sort(key=lambda x:x[0]*2+x[1], reverse=True)
+
+i = 0
+while aoki >= takahashi and i < n:
+    a, b = lst[i]
+    aoki -= a
+    takahashi += a+b
+    i += 1
+
+print(i)
