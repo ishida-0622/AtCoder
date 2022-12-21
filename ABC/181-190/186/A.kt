42579dcf2356dbfbd9521b7adf7eb719a8a80ea3
kentaro ishida <ken.babuo@gmail.com>
2022-12-22 01:43:48 +0900

    add:186_A~C

---
+++ ABC/181-190/186/A.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val (n, w) = readLine()!!.split(" ").map { it.toInt() }
+    println(n / w)
+}
---
+++ ABC/181-190/186/C.py
@@ -0,0 +1,35 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10**7)
+import bisect
+import string
+import math
+from math import gcd
+from copy import deepcopy
+from collections import deque, defaultdict
+from itertools import combinations, permutations, accumulate
+from functools import lru_cache
+from decimal import Decimal, ROUND_HALF_UP
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+max___ = max
+___min = min
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def lcm(a,b): return a*b // gcd(a,b)
+# lst.sort(key=lambda x: x[N]) <- N番目の要素でソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[0]) <- 辞書のkeyでソート
+# sorted_lst = sorted(lst.items(), key=lambda x: x[1]) <- 辞書のvalueでソート
+# Decimal(str(n)).quantize(Decimal("0.1"), ROUND_HALF_UP) <- 四捨五入
+
+
+
+n = int(input())
+ans = 0
+
+for i in range(1, n+1):
+    if "7" not in str(i) and "7" not in oct(i):
+        ans += 1
+
+print(ans)
