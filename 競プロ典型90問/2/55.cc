9fa892ab2f47124f328ff75a09434b4860e94135
kentaro ishida <ken.babuo@gmail.com>
2022-12-24 01:10:16 +0900

    add:55

---
+++ 競プロ典型90問/2/55.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define _rep(i, a, b) for (ll i = a, i##_len = (ll)(b); i < i##_len; i++)
+#define rep(i, n) _rep(i,0,n)
+#define reps(i, n) _rep(i,1,n+1)
+#define rrep(i, n) for (ll i = n-1; i >= 0; i--)
+#define rreps(i, n) for (ll i = n; i >= 1; i--)
+#define ALL(x) (x).begin(), (x).end()
+#define YesNo(flag) if(flag){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+#define _10_5 100010
+#define _2_10_5 200010
+#define mod 998244353
+#define mod2 1000000007
+#define inf 1000000000
+#define INF 1000000000000000000L
+// sort(ALL(lst),[](const vector<int> &alpha,const vector<int> &beta){return alpha[N] < beta[N];}); <- N番目の要素でソート
+
+
+
+int main(void){
+    ll N, P, Q;
+    cin >> N >> P >> Q;
+    vector<ll> lst(N);
+    rep(i, N) {
+        cin >> lst[i];
+    }
+    ll ans = 0;
+    rep(i, N) {
+        _rep(j, i+1, N) {
+            _rep(k, j+1, N) {
+                _rep(l, k+1, N) {
+                    _rep(m, l+1, N) {
+                        if (lst[i]*lst[j]%P*lst[k]%P*lst[l]%P*lst[m] % P == Q) ans++;
+                    }
+                }
+            }
+        }
+    }
+    cout << ans << endl;
+}
