f557bd78be5781ded0ebc10fb3590a242ca9ba00
kentaro ishida <ken.babuo@gmail.com>
2023-01-04 02:26:54 +0900

    update: 負閉路検出と経路復元の追加

--- templates/WarshallFloyd.py
+++ templates/WarshallFloyd.py
@@ -4,17 +4,42 @@ class WarshallFloyd:
     def __init__(self, n: int):
         self.n = n
         self.dist = [[inf] * n for _ in range(n)]
+        self.prev = [[-1] * n for _ in range(n)]
         for i in range(n):
             self.dist[i][i] = 0
 
     def union(self, x: int, y: int, cost: int = 1, is_directed: bool = False):
         self.dist[x][y] = cost
+        self.prev[x][y] = x
         if not is_directed:
             self.dist[y][x] = cost
+            self.prev[y][x] = y
 
     def warshall_floyd(self):
+        """ワーシャルフロイド法による探索 Ο(N^3)"""
+        is_negative = False
         for k in range(self.n):
             for i in range(self.n):
                 for j in range(self.n):
-                    self.dist[i][j] = min(self.dist[i][j], self.dist[i][k] + self.dist[k][j])
-        return self.dist
+                    if self.dist[i][k] + self.dist[k][j] < self.dist[i][j]:
+                        self.dist[i][j] = self.dist[i][k] + self.dist[k][j]
+                        self.prev[i][j] = self.prev[k][j]
+        # 負閉路検出
+        for i in range(self.n):
+            if self.dist[i][i] < 0:
+                is_negative = True
+        return self.dist, is_negative
+
+    def get_path(self, start: int, goal: int):
+        """
+        最短経路の復元\n
+        warshall_floydが実行されているのが前提
+        """
+        path = []
+        now = goal
+        while now != start:
+            path.append(now)
+            now = self.prev[start][now]
+        path.append(start)
+        path.reverse()
+        return path
