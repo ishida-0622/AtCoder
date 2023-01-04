d493b5ca49e875b9d9811beac39ecf9b1ef8fb25
kentaro ishida <ken.babuo@gmail.com>
2023-01-04 18:05:14 +0900

    add: 94_A~D

---
+++ ABC/91-100/94/C.py
@@ -0,0 +1,37 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(7*10**5)
+import string
+import math
+from math import gcd, sqrt
+from copy import deepcopy
+from collections import deque, defaultdict, Counter
+from itertools import combinations, permutations, accumulate
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+from heapq import heapify, heappop, heappush
+from functools import lru_cache
+from decimal import Decimal, ROUND_HALF_UP
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def lcm(a,b): return a*b // gcd(a,b)
+# Decimal(str(n)).quantize(Decimal("0.1"), ROUND_HALF_UP) <- 四捨五入
+
+
+
+n = int(input())
+lst = list(map(int, input().split()))
+sorted_lst = sorted(lst)
+a, b = sorted_lst[len(lst) // 2 - 1], sorted_lst[len(lst) // 2]
+
+if a == b:
+    for _ in range(n):
+        print(a)
+else:
+    for v in lst:
+        if v <= a:
+            print(b)
+        else:
+            print(a)
