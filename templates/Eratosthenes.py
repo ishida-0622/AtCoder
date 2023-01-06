adcd2ee340fac80f9b8ec539f4248503ecdf08b4
kentaro ishida <ken.babuo@gmail.com>
2023-01-07 02:50:46 +0900

    update: 計算量を追記

--- templates/Eratosthenes.py
+++ templates/Eratosthenes.py
@@ -1,5 +1,5 @@
 def Eratosthenes(N: int):
-    """エラトステネスの篩"""
+    """エラトステネスの篩. Ο(N log log N)"""
     is_prime = [True if i % 2 else False for i in range(N+1)]
     is_prime[1], is_prime[2] = False, True
 
