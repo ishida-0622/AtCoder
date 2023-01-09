db52071ff2f89c6784cabe4b22ddf12721cdb63c
kentaro ishida <ken.babuo@gmail.com>
2023-01-09 22:17:37 +0900

    add: 205_D

---
+++ ABC/201-210/205/D.py
@@ -0,0 +1,33 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(7*10**5)
+import string
+import math
+from math import gcd, sqrt
+from copy import deepcopy
+from collections import deque, defaultdict, Counter
+from itertools import combinations, permutations, accumulate
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+from heapq import heapify, heappop, heappush
+from functools import lru_cache
+from decimal import Decimal, ROUND_HALF_UP
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def lcm(a,b): return a*b // gcd(a,b)
+# Decimal(str(n)).quantize(Decimal("0.1"), ROUND_HALF_UP) <- 四捨五入
+
+
+
+N, Q = map(int, input().split())
+lst = sorted(list(map(int, input().split())))
+ls = [lst[0] - 1]
+
+for i in range(1,N):
+    ls.append(lst[i] - lst[i-1] + ls[i-1] - 1)
+
+for _ in range(Q):
+    k = int(input())
+    print(k + bisect_left(ls, k))
