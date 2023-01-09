e0fe44da645c376c8048bc271f5048d7e7101df8
kentaro ishida <ken.babuo@gmail.com>
2023-01-09 22:31:26 +0900

    add: 77_A~C

---
+++ ABC/71-80/77/C.py
@@ -0,0 +1,33 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(7*10**5)
+import string
+import math
+from math import gcd, sqrt
+from copy import deepcopy
+from collections import deque, defaultdict, Counter
+from itertools import combinations, permutations, accumulate
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+from heapq import heapify, heappop, heappush
+from functools import lru_cache
+from decimal import Decimal, ROUND_HALF_UP
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def lcm(a,b): return a*b // gcd(a,b)
+# Decimal(str(n)).quantize(Decimal("0.1"), ROUND_HALF_UP) <- 四捨五入
+
+
+
+n = int(input())
+A = sorted(list(map(int, input().split())))
+B = sorted(list(map(int, input().split())))
+C = sorted(list(map(int, input().split())))
+ans = 0
+
+for v in B:
+    ans += bisect_left(A, v) * (n - bisect_right(C, v))
+
+print(ans)
