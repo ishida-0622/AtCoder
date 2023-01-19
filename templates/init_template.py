e9837cb6d2c8d4b458e3a710463f6896572feaae
kentaro ishida <ken.babuo@gmail.com>
2023-01-19 11:22:58 +0900

    update

--- templates/init_template.py
+++ templates/init_template.py
@@ -16,6 +16,8 @@ mod2 = 1000000007
 inf = float("inf")
 minf = -float("inf")
 def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def yes(yes="Yes"): print(yes);exit()
+def no(no="No"): print(no);exit()
 def lcm(a,b): return a*b // gcd(a,b)
 # Decimal(str(n)).quantize(Decimal("0.1"), ROUND_HALF_UP) <- 四捨五入
 
