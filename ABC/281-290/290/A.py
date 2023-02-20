713c90ec95cb98dc81c5690812b370f67e7cb2a8
kentaro ishida <ken.babuo@gmail.com>
2023-02-20 23:07:29 +0900

    add: 290_A~C

---
+++ ABC/281-290/290/A.py
@@ -0,0 +1,29 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(7*10**5)
+import string
+import math
+from math import gcd, sqrt
+from copy import deepcopy
+from collections import deque, defaultdict, Counter
+from itertools import combinations, permutations, accumulate
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+from heapq import heapify, heappop, heappush
+from functools import lru_cache
+from decimal import Decimal, ROUND_HALF_UP
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def yes(yes="Yes"): print(yes);exit()
+def no(no="No"): print(no);exit()
+def lcm(a,b): return a*b // gcd(a,b)
+# Decimal(str(n)).quantize(Decimal("0.1"), ROUND_HALF_UP) <- 四捨五入
+
+
+
+n, m = map(int, input().split())
+lst = list(map(int, input().split()))
+B = set(map(int, input().split()))
+print(sum([v for i, v in enumerate(lst) if i+1 in B]))
