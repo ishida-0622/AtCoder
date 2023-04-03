4fdb8fb3141e1dddbdb76246d328bfe54ba292cd
kentaro ishida <ken.babuo@gmail.com>
2023-04-03 11:49:50 +0900

    add: 174_A~D

---
+++ ABC/171-180/174/D.py
@@ -0,0 +1,43 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(7*10**5)
+import string
+import math
+from math import gcd, sqrt, floor, ceil, sin, cos, tan, pi
+from copy import deepcopy
+from collections import deque, defaultdict, Counter
+from itertools import combinations, permutations, accumulate
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+from heapq import heapify, heappop, heappush
+from functools import lru_cache, reduce
+from decimal import Decimal, ROUND_HALF_UP
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+az = string.ascii_lowercase
+AZ = string.ascii_uppercase
+aZ = string.ascii_letters
+zero_nine = string.digits
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def yes(yes="Yes"): print(yes);exit()
+def no(no="No"): print(no);exit()
+def lcm(a,b): return a*b // gcd(a,b)
+
+
+
+n = int(input())
+lst = input()
+i, j = 0, n-1
+ans = 0
+
+while i < j:
+    while i < n-1 and lst[i] == "R":
+        i += 1
+    while j > 0 and lst[j] == "W":
+        j -= 1
+    if i < j:
+        ans += 1
+        i += 1
+        j -= 1
+print(ans)
