dbde272499762778200632b79e5eacad8a48d562
kentaro ishida <ken.babuo@gmail.com>
2023-04-11 09:59:34 +0900

    add: 297_A~D

---
+++ ABC/291-300/297/A.py
@@ -0,0 +1,39 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(7*10**5)
+import string
+import math
+from math import gcd, sqrt, floor, ceil, sin, cos, tan, pi
+from copy import deepcopy
+from collections import deque, defaultdict, Counter
+from itertools import combinations, permutations, accumulate, groupby
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+from heapq import heapify, heappop, heappush
+from functools import lru_cache, reduce
+from decimal import Decimal, ROUND_HALF_UP
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+az = string.ascii_lowercase
+AZ = string.ascii_uppercase
+aZ = string.ascii_letters
+zero_nine = string.digits
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def yes(yes="Yes"): print(yes);exit()
+def no(no="No"): print(no);exit()
+def lcm(a: int, b: int): return a*b // gcd(a,b)
+def round(n: int, d: int = 0): return float(Decimal(n).quantize(Decimal(f"1e{d}"), ROUND_HALF_UP))
+
+
+
+def main():
+    n, d = map(int, input().split())
+    lst = list(map(int, input().split()))
+    for i in range(n-1):
+        if lst[i+1] - lst[i] <= d:
+            yes(lst[i+1])
+    no(-1)
+
+if __name__ == "__main__":
+    main()
---
+++ ABC/291-300/297/C.py
@@ -0,0 +1,42 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(7*10**5)
+import string
+import math
+from math import gcd, sqrt, floor, ceil, sin, cos, tan, pi
+from copy import deepcopy
+from collections import deque, defaultdict, Counter
+from itertools import combinations, permutations, accumulate, groupby
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+from heapq import heapify, heappop, heappush
+from functools import lru_cache, reduce
+from decimal import Decimal, ROUND_HALF_UP
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+az = string.ascii_lowercase
+AZ = string.ascii_uppercase
+aZ = string.ascii_letters
+zero_nine = string.digits
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def yes(yes="Yes"): print(yes);exit()
+def no(no="No"): print(no);exit()
+def lcm(a: int, b: int): return a*b // gcd(a,b)
+def round(n: int, d: int = 0): return float(Decimal(n).quantize(Decimal(f"1e{d}"), ROUND_HALF_UP))
+
+
+
+def main():
+    h, w = map(int, input().split())
+    lst = [list(input()) for _ in range(h)]
+    for i in range(h):
+        for j in range(w-1):
+            if lst[i][j] == "T" and lst[i][j+1] == "T":
+                lst[i][j] = "P"
+                lst[i][j+1] = "C"
+    for v in lst:
+        print(*v, sep="")
+
+if __name__ == "__main__":
+    main()
