ab51a5f9b5e70bfdd18a4e0c64ced24c773085d2
kentaro ishida <ken.babuo@gmail.com>
2023-04-20 09:49:19 +0900

    add: 168_A~C

---
+++ ABC/161-170/168/C.py
@@ -0,0 +1,43 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(7*10**5)
+import string
+import math
+from math import gcd, sqrt, floor, ceil, sin, cos, tan, radians, pi
+from copy import deepcopy
+from collections import deque, defaultdict, Counter
+from itertools import combinations, permutations, accumulate, groupby
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+from heapq import heapify, heappop, heappush
+from functools import lru_cache, reduce
+from decimal import Decimal, ROUND_HALF_UP
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+az = string.ascii_lowercase
+AZ = string.ascii_uppercase
+aZ = string.ascii_letters
+zero_nine = string.digits
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def yes(yes="Yes"): print(yes);exit()
+def no(no="No"): print(no);exit()
+def lcm(a: int, b: int): return a*b // gcd(a,b)
+def round(n: int, d: int = 0): return float(Decimal(n).quantize(Decimal(f"1e{d}"), ROUND_HALF_UP))
+
+
+
+def main():
+    A, B, H, M = map(int, input().split())
+    a = 360 * ((H + M / 60) / 12)
+    b = 360 * (M / 60)
+    a_rad = radians(a)
+    b_rad = radians(b)
+    a_rotated = 0 * cos(a_rad) - A * sin(a_rad)
+    a_rotated_ = 0 * sin(a_rad) + A * cos(a_rad)
+    b_rotated = 0 * cos(b_rad) - B * sin(b_rad)
+    b_rotated_ = 0 * sin(b_rad) + B * cos(b_rad)
+    print(f"{sqrt((a_rotated - b_rotated) ** 2 + (a_rotated_ - b_rotated_) ** 2):.11f}")
+
+if __name__ == "__main__":
+    main()
