d503b845c5092d82afd9e689e3a806adfdff1e2d
kentaro ishida <ken.babuo@gmail.com>
2023-04-20 09:48:22 +0900

    add: 61

---
+++ 競プロ典型90問/2/61.py
@@ -0,0 +1,39 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(7*10**5)
+import string
+import math
+from math import gcd, sqrt, floor, ceil, sin, cos, tan, pi
+from copy import deepcopy
+from collections import deque, defaultdict, Counter
+from itertools import combinations, permutations, accumulate
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+from heapq import heapify, heappop, heappush
+from functools import lru_cache, reduce
+from decimal import Decimal, ROUND_HALF_UP
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+az = string.ascii_lowercase
+AZ = string.ascii_uppercase
+aZ = string.ascii_letters
+zero_nine = string.digits
+def YesNo(flag: bool, yes="Yes", no="No"): print(yes) if flag else print(no)
+def yes(yes="Yes"): print(yes);exit()
+def no(no="No"): print(no);exit()
+def lcm(a,b): return a*b // gcd(a,b)
+# Decimal(str(n)).quantize(Decimal("0.1"), ROUND_HALF_UP) <- 四捨五入
+
+
+
+Q = int(input())
+que = deque([])
+for _ in range(Q):
+    t, x = map(int, input().split())
+    if t == 1:
+        que.appendleft(x)
+    elif t == 2:
+        que.append(x)
+    else:
+        print(que[x-1])
