fe7ebe3e78f8ada22aae4fcfde8b350605f66d76
kentaro ishida <ken.babuo@gmail.com>
2023-06-05 10:55:10 +0900

    update

--- template/init_template.py
+++ template/init_template.py
@@ -44,18 +44,6 @@ def round(n: int, d: int = 0):
     return float(Decimal(n).quantize(Decimal(f"1e{d}"), ROUND_HALF_UP))
 
 
-def base_n(num_10: int, n: int):
-    """10進数 -> n進数変換(n<=36)"""
-    if num_10 == 0:
-        return "0"
-    s = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    res = ""
-    while num_10:
-        res += s[num_10 % n]
-        num_10 //= n
-    return res[::-1]
-
-
 def main():
     return
 
