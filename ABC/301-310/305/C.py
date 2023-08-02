e2909878e91040e0707024fcd2c86258f9f7af24
kentaro ishida <ken.babuo@gmail.com>
2023-08-03 03:13:04 +0900

    add: 305_A~C

---
+++ ABC/301-310/305/C.py
@@ -0,0 +1,68 @@
+import sys
+import string
+import math
+from math import gcd, sqrt, floor, ceil, radians, sin, cos, tan, pi
+from copy import deepcopy
+from collections import deque, defaultdict, Counter
+from itertools import combinations, permutations, accumulate, groupby
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+from heapq import heapify, heappop, heappush
+from functools import lru_cache, reduce
+from decimal import Decimal, ROUND_HALF_UP
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(7 * 10**5)
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+az = string.ascii_lowercase
+AZ = string.ascii_uppercase
+aZ = string.ascii_letters
+zero_nine = string.digits
+
+
+def YesNo(flag: bool, yes="Yes", no="No"):
+    print(yes) if flag else print(no)
+
+
+def yes(yes="Yes"):
+    print(yes)
+    exit()
+
+
+def no(no="No"):
+    print(no)
+    exit()
+
+
+def lcm(a: int, b: int):
+    return a * b // gcd(a, b)
+
+
+def round(n: int, d: int = 0):
+    return float(Decimal(n).quantize(Decimal(f"1e{d}"), ROUND_HALF_UP))
+
+
+def main():
+    h, w = map(int, input().split())
+    lst = [list(input()) for _ in range(h)]
+    d = ((1, 0), (-1, 0), (0, 1), (0, -1))
+    for i in range(h):
+        for j in range(w):
+            if lst[i][j] == "#":
+                continue
+            cnt = 0
+            for a, b in d:
+                try:
+                    if lst[i + a][j + b] == "#":
+                        cnt += 1
+                except IndexError:
+                    pass
+            if cnt > 1:
+                print(i + 1, j + 1)
+                exit()
+
+
+if __name__ == "__main__":
+    main()
