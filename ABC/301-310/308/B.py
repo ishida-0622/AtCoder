16e1c05ac4cc790f21c8f7abca86d3096b597e40
kentaro ishida <ken.babuo@gmail.com>
2023-09-26 10:30:31 +0900

    add: 308_A~C

---
+++ ABC/301-310/308/B.py
@@ -0,0 +1,57 @@
+import sys
+import string
+import math
+from math import gcd, sqrt, floor, ceil, radians, sin, cos, tan, pi
+from copy import deepcopy
+from collections import deque, defaultdict, Counter
+from itertools import combinations, permutations, accumulate, groupby
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+from heapq import heapify, heappop, heappush
+from functools import lru_cache, reduce
+from decimal import Decimal, ROUND_HALF_UP
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(7 * 10**5)
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+az = string.ascii_lowercase
+AZ = string.ascii_uppercase
+aZ = string.ascii_letters
+zero_nine = string.digits
+
+
+def YesNo(flag: bool, yes="Yes", no="No"):
+    print(yes) if flag else print(no)
+
+
+def yes(yes="Yes"):
+    print(yes)
+    exit()
+
+
+def no(no="No"):
+    print(no)
+    exit()
+
+
+def lcm(a: int, b: int):
+    return a * b // gcd(a, b)
+
+
+def round(n: int, d: int = 0):
+    return float(Decimal(n).quantize(Decimal(f"1e{d}"), ROUND_HALF_UP))
+
+
+def main():
+    n, m = map(int, input().split())
+    C = input().split()
+    D = input().split()
+    P = list(map(int, input().split()))
+    dic = {D[i]: P[i + 1] for i in range(m)}
+    print(sum([dic.get(v, P[0]) for v in C]))
+
+
+if __name__ == "__main__":
+    main()
---
+++ ABC/301-310/308/C.py
@@ -0,0 +1,63 @@
+import math
+import string
+import sys
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from decimal import ROUND_HALF_UP, Decimal, getcontext
+from functools import lru_cache, reduce
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, combinations, groupby, permutations
+from math import ceil, cos, floor, gcd, pi, radians, sin, sqrt, tan
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(7 * 10**5)
+mod = 998244353
+mod2 = 1000000007
+inf = float("inf")
+minf = -float("inf")
+az = string.ascii_lowercase
+AZ = string.ascii_uppercase
+aZ = string.ascii_letters
+zero_nine = string.digits
+getcontext().prec = 100
+
+
+def YesNo(flag: bool, yes="Yes", no="No"):
+    print(yes) if flag else print(no)
+
+
+def yes(yes="Yes"):
+    print(yes)
+    exit()
+
+
+def no(no="No"):
+    print(no)
+    exit()
+
+
+def lcm(a: int, b: int):
+    return a * b // gcd(a, b)
+
+
+def round(n: int, d: int = 0):
+    return float(Decimal(n).quantize(Decimal(f"1e{d}"), ROUND_HALF_UP))
+
+
+def f(arg):
+    a, b, _ = arg
+    a = Decimal(a)
+    b = Decimal(b)
+    return -a / (a + b)
+
+
+def main():
+    n = int(input())
+    lst = [list(map(int, input().split())) + [i + 1] for i in range(n)]
+    lst.sort(key=f)
+    print(*list(zip(*lst))[2])
+
+
+if __name__ == "__main__":
+    main()
